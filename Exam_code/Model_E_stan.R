22c353883eddc02265efc63a26a217e1fabdedca
Andrey776 <61335235+Andrey776@users.noreply.github.com>
2020-12-07 00:20:09 -0700

    time to sleep

--- Exam_code/Model_E_stan.R
+++ Exam_code/Model_E_stan.R
@@ -27,19 +27,23 @@ parameters {
   real beta1;
   real beta2;
 }
+transformed parameters {
+  real log_lambda[T];
+  for (i in 1:T) {
+   log_lambda[i] = log_pop[i] + beta0 + beta1 * income[i] + beta2 * bs[i];
+  }
+
+}
 
 model {
-  real log_lambda[T];
-
   beta0 ~ normal(0, 5);
   beta1 ~ normal(0, 5);
   beta2 ~ normal(0, 5);
  
   for (i in 1:T) {
-    log_lambda[i] = log_pop[i] + beta0 + beta1 * income[i] + beta2 * bs[i];
     deaths[i] ~ poisson_log(log_lambda[i]);
   }
-
+}
 generated quantities {
   vector[T] log_lik;
 
@@ -63,9 +67,35 @@ r_fit_E = stan(model_code = stan_mod_E, data = stan_dat_E,
 
 summary(r_fit_E, pars = c("beta0", "beta1", "beta2"), prob = c(0.025, 0.975))$summary
 
-#sso <- launch_shinystan(r_fit_E)
+sso <- launch_shinystan(r_fit_E)
 file_name = paste(model_name, as.character(Sys.Date()), ".rda", sep="")
-#setwd('/Users/AM/Documents/_CU Masters/2020 fall Bayesian_7393/code/Bayesian_Statistics_Class_Code/Exam_code')
+setwd('/Users/AM/Documents/_CU Masters/2020 fall Bayesian_7393/code/Bayesian_Statistics_Class_Code/Exam_code')
 #readRDS(r_fit, file = "") 
-#saveRDS(r_fit_E, file = file_name, compress = "xz") 
+saveRDS(r_fit_E, file = file_name, compress = "xz") 
+waic_loo = function (fit) {
+  log_lik = extract_log_lik(fit, merge_chains = FALSE)
+  r_eff = exp(relative_eff(log_lik))
+  wl = c(waic(log_lik)$waic, loo(log_lik, r_eff = r_eff)$looic)
+  return(wl)
+}
+
+cat("STAN Model E WAIC", waic_loo(r_fit_E)[1])
+cat("STAN Model E LOOIC", waic_loo(r_fit_E)[2])
+
+
+samples = extract(r_fit_E)
+ncycles = length(samples[[1]])
+T <- length(df_covid$bs)
+
+# each row of yrep is a sample from the pp distribution
+yrep = matrix(0, ncol = T, nrow = ncycles)
+for (i in seq_len(T)) {
+  log_l = log(df_covid$population[i]) +
+    samples$beta0 + 
+    samples$beta1 * df_covid$income[i] + 
+    samples$beta2 * df_covid$bs[i]
+  yrep[, i] = rpois(ncycles, lambda = exp(log_l))
+}
+
+ppc_intervals(y = df_covid$deaths, yrep = yrep) + ggtitle("Model A: Posterior predictive intervals to the observed data values") + xlab("State") + ylab("Covid deaths")
 
